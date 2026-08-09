a8828f3f9a0fd8fdaadc939eefcb6c7d29874cbb
piotr.konopacki <konopackipio@gmail.com>
2026-08-09 16:36:26 +0200

    Added file for loading configurations

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -2,8 +2,7 @@ import { loadEnvConfig } from '../config/config';
 
 function main(): void {
   const config = loadEnvConfig();
-  console.log('Loaded environment configuration:', config);
-  // console.log('second-brain-assistant: scaffold ready. MVP not implemented yet.');
+  console.log('Loaded environment configuration');
 }
 
 main();
